6b585ef5c85ced40399f5144b41e182a138d051c
NerdioN <neel99khalade@gmail.com>
2020-09-05 14:13:12 +0530

    handled history API feedback

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+var history = require('connect-history-api-fallback');
 
 const app = express()
-
-app.use('/*',serveStatic(path.join(__dirname,'/dist')))
-
-app.use(history({
-    // OPTIONAL: Includes more verbose logging
-    verbose: true
-}))
+app.use(history())
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
 const port = process.env.PORT || 8080
 app.listen(port)
